6c7b75182ae145566df2a4c35cce565d6b0721eb
Студент <@5-115P-05>
2022-06-01 14:00:38 +0300

    Добавьте файлы проекта.

---
+++ Window2.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Warehouse
+{
+    /// <summary>
+    /// Логика взаимодействия для Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void bb2_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow win0 = new MainWindow();
+            win0.Show();
+            Close();
+        }
+    }
+}
---
+++ Window5.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Warehouse
+{
+    /// <summary>
+    /// Логика взаимодействия для Window5.xaml
+    /// </summary>
+    public partial class Window5 : Window
+    {
+        public Window5()
+        {
+            InitializeComponent();
+        }
+
+        private void bb5_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow win0 = new MainWindow();
+            win0.Show();
+            Close();
+        }
+    }
+}
